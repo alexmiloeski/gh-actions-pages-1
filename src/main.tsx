f0f1cfb7597f1ce025679053cd257136e5c8cca1
alexmiloeski <a.miloeski@gmail.com>
2024-03-10 14:09:26 +0100

    replace BrowserRouter with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import Home from "./components/Home.tsx";
@@ -8,7 +8,7 @@ import { Help } from "./components/Help.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router basename="/gh-actions-pages-1/">
+    <Router>
       <App />
       <Routes>
         <Route path="/" element={<Home />} />
